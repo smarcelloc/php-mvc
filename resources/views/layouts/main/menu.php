fb3139e403f3a3d357c58940adaa198e7bde5a9f
Marcello Silvério <smarcelloc@gmail.com>
2022-02-05 12:31:57 -0300

    chore: create layout in View

---
+++ resources/views/layouts/main/menu.php
@@ -0,0 +1,21 @@
+<nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#"><?= APP_NAME ?></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="<?= APP_URL ?>">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="<?= APP_URL ?>/about">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="<?= APP_URL ?>/testimonials">Testimonials</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
