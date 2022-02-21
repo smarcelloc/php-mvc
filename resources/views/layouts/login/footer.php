f157b42206285481de14c4f377a04c659561023c
Marcello Silvério <smarcelloc@gmail.com>
2022-02-21 12:49:38 -0300

    feat: add layout Login

---
+++ resources/views/layouts/login/footer.php
@@ -0,0 +1,3 @@
+<footer class="pt-3 mt-4 text-muted border-top text-center">
+    Copyright © <?= date('Y') ?> - <?= APP_NAME ?>
+</footer>
---
+++ resources/views/layouts/login/header.php
@@ -0,0 +1,4 @@
+<header class="text-center mb-3">
+    <h1><?= APP_NAME ?></h1>
+    <h1 class="h3 mb-3 fw-normal"><?= $title ?></h1>
+</header>
